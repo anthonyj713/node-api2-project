c9860abb201c87d8cb7844856aa178693404d1a3
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-05-05 16:39:34 -0600

    get post requests are working

--- data/posts/posts-router.js
+++ data/posts/posts-router.js
@@ -10,11 +10,31 @@ router.get('/', (req, res) => {
         res.status(200).json(posts)
     })
     .catch(err => {
+        console.log(err)
         res.status(500).json({
             message: 'Error retrieving the posts'
-        })      
+        });      
+    });
+});
+
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+    .then(post => {
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({
+                message: 'Post not found'
+            });
+        }
     })
-})
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            message: "Error retrieving the post"
+        });
+    });
+});
 
 
 
