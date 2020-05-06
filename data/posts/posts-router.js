231b101f7a8ec7148c6d4ff0d377915c726af3e2
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-05-06 10:04:50 -0600

    MVP met

--- data/posts/posts-router.js
+++ data/posts/posts-router.js
@@ -52,9 +52,98 @@ router.get('/:id/comments', (req, res) => {
       res.status(500).json({
           message: 'The comments information could not be retrieved.'
       });
-  })
-})
+  });
+});
 
+router.post('/', (req, res) => {
+    if(!req.body.title && !req.body.contents){
+        res.status(400).json({
+            message:'Please provide a title and contents for the post'
+        })
+    } else {
+        Posts.insert(req.body)
+        .then(post => {
+            res.status(201).json(post);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: 'There was an error while saving the post to the database'
+            });
+        });
+    };
+});
+
+router.post('/:id/comments', (req, res) => {
+    console.log(req.body)
+    if(Posts.findById(req.params.id)){
+        if(!req.body.text){
+            res.status(400).json({
+                message:'Please provide text for the comment.'
+            })
+        } else {
+            Posts.insertComment(req.body)
+            .then(comment => {
+                res.status(201).json(comment)
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    message:'There was an error while saving the comment to the database.'
+                })
+            })
+        }
+    } else {
+        res.status(404).json({
+            message:'The post with the specified ID does not exist.'
+            });
+    };
+    
+});
+
+router.delete('/:id', (req, res) => {
+    Posts.remove(req.params.id)
+    .then(count => {
+        if (count > 0) {
+            res.status(200).json({
+                message: 'The post has been deleted'
+            })
+        } else {
+            res.status(404).json({
+                message: 'The post with the specified id cannot be found'
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            message: 'Error removing the post'
+        });
+    });
+});
+
+router.put('/:id', (req,res) => {
+    if(!req.body.title && !req.body.contents){
+        res.status(400).json({
+            message: 'Please provide a title and content'
+        })
+    } else {
+        Posts.update(req.params.id, req.body)
+        .then(count => {
+            if(count > 0){
+                res.status(201).json({
+                    message: 'Post successfully updated'
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: 'Error updating post'
+            });
+        });
+    };
+});
 
 
 module.exports = router;
